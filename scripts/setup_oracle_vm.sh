9c0f942b9b69d82254a19a47cacad8617d1ba3bc
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2023-11-13 16:10:10 -0300

    fix setup docker install

--- scripts/setup_oracle_vm.sh
+++ scripts/setup_oracle_vm.sh
@@ -7,7 +7,14 @@ sudo NEEDRESTART_MODE=a apt-get upgrade -y
 
 # Docker Swarm:
 sudo apt-get install vim cron apache2-utils -y
-curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh && rm get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh
+chmod +x get-docker.sh
+sudo ./get-docker.sh
+rm get-docker.sh
+# sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+# apt-cache policy docker-ce
 sudo usermod -aG docker $USER
 
 echo "docker installed successfully"
